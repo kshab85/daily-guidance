66785d23ba86863e0bf206be9dbbb2e0dd05c5bf
shabAkos2021 <shabina@akosweb.com>
2022-01-05 18:28:19 +0530

    Updates

---
+++ precache-manifest.6a605769d5dacc9e41b6e0404708034a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4a081da7dcacb4852a",
+    "url": "/daily-guidance/static/css/main.5d9ff473.chunk.css"
+  },
+  {
+    "revision": "ce4a081da7dcacb4852a",
+    "url": "/daily-guidance/static/js/main.ce4a081d.chunk.js"
+  },
+  {
+    "revision": "6d4bcd68f1fa94d3e8a7",
+    "url": "/daily-guidance/static/js/1.6d4bcd68.chunk.js"
+  },
+  {
+    "revision": "d30194790309cf0efd24",
+    "url": "/daily-guidance/static/js/2.d3019479.chunk.js"
+  },
+  {
+    "revision": "7f838ae7ce880cc5df8d",
+    "url": "/daily-guidance/static/js/runtime~main.7f838ae7.js"
+  },
+  {
+    "revision": "39c6235c05b3f54f5d8fd30b01a989d1",
+    "url": "/daily-guidance/static/media/guidance.39c6235c.png"
+  },
+  {
+    "revision": "41c75c6432d02456facfa51994b9ee62",
+    "url": "/daily-guidance/static/media/sun.41c75c64.png"
+  },
+  {
+    "revision": "2be6db0ceb243ab48c846acdea7447df",
+    "url": "/daily-guidance/index.html"
+  }
+];
